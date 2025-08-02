01c340fa4f68cbf9139bfe4e7f11e34b115f8638
almamun2b <almamun2b@gmail.com>
2025-08-02 22:00:13 +0600

    updated

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,7 +1,5 @@
 import { Router } from "express";
 import { AuthRoute } from "../modules/auth/auth.route";
-import { DivisionRoutes } from "../modules/division/division.route";
-import { TourRoutes } from "../modules/tour/tour.route";
 import { TransactionRoutes } from "../modules/transaction/transaction.route";
 import { UserRoute } from "../modules/user/user.route";
 import { WalletRoutes } from "../modules/wallet/wallet.route";
@@ -16,14 +14,6 @@ const moduleRoutes = [
   {
     path: "/auth",
     route: AuthRoute,
-  },
-  {
-    path: "/division",
-    route: DivisionRoutes,
-  },
-  {
-    path: "/tour",
-    route: TourRoutes,
   },
   {
     path: "/wallet",
